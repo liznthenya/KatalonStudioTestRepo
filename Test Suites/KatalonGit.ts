856d7c447aa957796443e3d2e69000f3a2e68157
liznthenya <liznthenya@users.noreply.github.com>
2018-01-02 13:39:49 +0300

    TestSuit Commit

---
+++ Test Suites/KatalonGit.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatalonGit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-01-02T13:02:39</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d481d97c-9c31-4472-9b3f-4ffd256d8008</testSuiteGuid>
+</TestSuiteEntity>

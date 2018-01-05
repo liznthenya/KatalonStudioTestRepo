215f95cf9aceac3d7a8b88e503086f81cbf7d4c1
liznthenya <liznthenya@users.noreply.github.com>
2018-01-05 17:57:22 +0300

    Add Test Case 2 to test suite

--- Test Suites/KatalonGit.ts
+++ Test Suites/KatalonGit.ts
@@ -4,7 +4,7 @@
    <name>KatalonGit</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-01-05T17:51:31</lastRun>
+   <lastRun>2018-01-05T17:56:33</lastRun>
    <mailRecipient>liznthenya@gmail.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
